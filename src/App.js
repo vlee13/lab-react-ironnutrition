594bb6a0cc0a5c119b3c78015d3bcf93b487e336
vlee13 <vlee13@gmail.com>
2020-07-01 13:53:18 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import FoodBox from './Components/FoodBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    showTheFoods: foods,
+    showForm: true,
+  };
+
+  showFoods = () => {
+    return this.state.showTheFoods.map((eachFood) => {
+      return (
+        <FoodBox
+          name={eachFood.name}
+          image={eachFood.image}
+          calories={eachFood.calories}
+        />
+      );
+    });
+  };
+
+  toggleForm = () => {
+    this.setState({
+      showForm: !this.state.showForm,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let newFood = {
+      name: this.state.name,
+      calories: this.state.calories,
+      image: this.state.image,
+    };
+    let copyOfFoods = [...this.state.showTheFoods];
+
+    copyOfFoods.unshift(newFood);
+    this.setState({
+      showForm: false,
+      showTheFoods: copyOfFoods,
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      showForm: !this.state.showForm,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>IronNutrition</h1>
+        <input onChange={this.handleChange} type="text" placeholder="search" />
+        {this.showFoods()}
+        <button onClick={this.toggleForm}>Toggle Form</button>
+        {this.state.showForm ? (
+          <form onSubmit={this.handleSubmit}>
+            <input
+              onChange={this.handleChange}
+              placeholder="name"
+              name="name"
+              type="text"
+            />
+            <input
+              onChange={this.handleChange}
+              placeholder="name"
+              name="name"
+              type="text"
+            />
+            <input
+              onChange={this.handleChange}
+              placeholder="name"
+              name="name"
+              type="text"
+            />
+            <input type="submit" />
+          </form>
+        ) : (
+          ''
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
